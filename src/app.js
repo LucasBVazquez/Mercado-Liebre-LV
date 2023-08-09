a83fedce96c93e3d1e12b076f526e924ce009d0b
Lucas450B <lucasvazquez828@gmail.com>
2023-08-09 11:30:24 -0300

    haciendo el render

--- src/app.js
+++ src/app.js
@@ -9,10 +9,8 @@ const path = require("path");
 app.use(express.static("public"));
 
 // Ponemos a escuchar el servidor
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000")
-});
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 // Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
 app.get("/", (req, res) => {
     let htmlPath = path.resolve(__dirname, "./views/home.html");
